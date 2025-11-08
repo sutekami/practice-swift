a9eea1aaea7ff30400ce85d31fe5ef03ad818e3b
sutekami <gigamasi@gmail.com>
2025-11-08 12:53:11 +0900

    add: new practice project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by 儀賀真周 on 2025/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by 儀賀真周 on 2025/11/08.
+//
+
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
