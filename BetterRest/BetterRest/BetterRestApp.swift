0aa00738943fde92cd8210a65fd08d248084fce6
Mashu Giga <gigamasi@gmail.com>
2025-11-10 19:52:40 +0900

    add: study content of BetterRest

--- BetterRest/BetterRest/BetterRestApp.swift
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct BetterRestApp: App {
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
-  }
 }
