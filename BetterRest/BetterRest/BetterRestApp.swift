a9e89d5cc308664261fb94dff7fdc8c5a7afe500
Mashu-Giga <mashu-giga@Mashu-GiganoMac-mini.local>
2025-11-10 18:51:55 +0900

    add: new practice

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mashu-Giga on 2025/11/10.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
